f6a4aae2002c28d900eb7b888d4da9c66beb2b1c
ubuntu <ubuntu@localhost.localdomain>
2022-11-01 23:30:30 +0800

    design some structure and impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
-enum task_state {
+#[derive(Debug)]
+enum taskState {
     abandon,
     done,
     none,
 }
 
+#[derive(Debug)]
+struct Task {
+    content: String,
+    status: taskState,
+    comments: Option<String>,
+    create_time: String,
+    update_time: String,
+}
+
+#[derive(Debug)]
+struct TaskGroup {
+    tasks: Vec<Task>,
+}
+
+impl TaskGroup {
+    fn new() -> Self {
+        Self {
+            tasks: Vec::new()
+        }
+    }
+    fn import() -> Self {
+        Self {
+            tasks: Vec::new()
+        }
+    }
+    fn add(content: String, comments: Option<String>) -> bool {
+        true
+    }
+    fn delete(id: i32) -> bool {
+        true
+    }
+}
+
 fn main() {
+    println!("Hello, Wolrd");
 }
