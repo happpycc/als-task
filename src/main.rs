0dd50e3ce6a18e3c2e87c9391dfd4c89a2e4e388
ubuntu <ubuntu@localhost.localdomain>
2022-11-02 23:17:28 +0800

    design well the struct, maybe...

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
 #[derive(Debug)]
-enum taskState {
-    abandon,
-    done,
-    none,
+enum TaskState{
+    Abandon,
+    Done,
+    Todo,
 }
 
 #[derive(Debug)]
 struct Task {
     content: String,
-    status: taskState,
+    status: TaskState,
     comments: Option<String>,
     create_time: String,
     update_time: String,
+    deadline: String,
 }
 
 #[derive(Debug)]
 struct TaskGroup {
+    group_name: String,
     tasks: Vec<Task>,
 }
 
 impl TaskGroup {
-    fn new() -> Self {
+    fn new(group_name: String) -> Self {
         Self {
+            group_name: group_name,
             tasks: Vec::new()
         }
     }
-    fn import() -> Self {
+    fn import(group_name: String) -> Self {
         Self {
+            group_name: group_name,
             tasks: Vec::new()
         }
     }
@@ -39,5 +43,8 @@ impl TaskGroup {
 }
 
 fn main() {
-    println!("Hello, Wolrd");
+    let mut task_groups: Vec<TaskGroup> = vec![];
+    let homeless_group = TaskGroup::new("homeless".to_owned());
+    task_groups.push(homeless_group);
+    println!("{:?}", task_groups);
 }
